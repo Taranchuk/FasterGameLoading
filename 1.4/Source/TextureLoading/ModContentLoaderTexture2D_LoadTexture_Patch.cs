7bb5f886fbf0f71ef9c42e0675d275e460c7a059
Taranchuk <nfhfyxer@gmail.com>
2022-11-26 14:06:14 +0200

    fix

--- 1.4/Source/TextureLoading/ModContentLoaderTexture2D_LoadTexture_Patch.cs
+++ 1.4/Source/TextureLoading/ModContentLoaderTexture2D_LoadTexture_Patch.cs
@@ -8,12 +8,11 @@ using Verse;
 
 namespace FasterGameLoading
 {
-    [HarmonyPatch(typeof(ModContentLoader<Texture2D>), "LoadTexture")]
+ [HarmonyPatch(typeof(ModContentLoader<Texture2D>), "LoadTexture")]
     public static class ModContentLoaderTexture2D_LoadTexture_Patch
     {
         public static Dictionary<string, string> loadedTexturesThisSession = new Dictionary<string, string>();
         public static Dictionary<string, Texture2D> savedTextures = new Dictionary<string, Texture2D>();
-        public static Texture2D stubTexture;
         public static bool Prefix(VirtualFile file, out bool __state, ref Texture2D __result)
         {
             if (savedTextures.TryGetValue(file.FullPath, out __result))
@@ -21,6 +20,7 @@ namespace FasterGameLoading
                 __state = false;
                 return false;
             }
+
             __state = true;
             var fullPath = file.FullPath;
             var index = fullPath.IndexOf("Textures\\");
@@ -30,16 +30,15 @@ namespace FasterGameLoading
                 loadedTexturesThisSession[path] = file.FullPath;
                 if (FasterGameLoadingSettings.loadedTexturesSinceLastSession.TryGetValue(path, out var otherPath) && fullPath != otherPath)
                 {
-                    if (stubTexture is null)
-                    {
-                        stubTexture = new Texture2D(2, 2);
-                    }
-                    __result = stubTexture;
+                    var texture = new Texture2D(2, 2);
+                    texture.name = Path.GetFileNameWithoutExtension(file.Name);
+                    __result = texture;
                     return false;
                 }
             }
             return true;
         }
+
         public static void Postfix(VirtualFile file, bool __state, Texture2D __result)
         {
             if (__state)
